076a68c9b3d590cee0af6218efc829d211bf5772
9oOzv <miika.piiroinen@gmail.com>
2025-01-04 16:12:20 +0200

    Fix service / podman yaml / start scripts

---
+++ server.sh
@@ -0,0 +1,17 @@
+set -eu
+
+if [ -f ./env.sh ]; then
+    . ./env.sh
+fi
+
+if [ ! -d ./node_modules ]; then
+    npm install
+fi
+
+if ! npm outdated; then
+    npm update
+fi
+
+npm audit fix
+
+node server.mjs "$@"
